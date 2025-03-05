040a9bbd078a75c0d32d7503b12684f9f8150b64
Che-somruk <somruk0504@gmail.com>
2025-03-05 22:15:25 +0700

    update setStringHdp

--- src/app/api/setStringHdp/route.ts
+++ src/app/api/setStringHdp/route.ts
@@ -1,18 +1,25 @@
 import { NextResponse } from "next/server";
 
 export function setStringHdp(number: number): string {
-  if (number == Math.floor(number)) {
-    return number === 0 ? "0" : `${number}.0`;
-  }
+    if (number === Math.floor(number)) {
+        return number === 0 ? "0" : `${number}.0`;
+    }
 
-  const base = Math.floor(number);
-  const decimal = number - base;
+    const base = Math.floor(number);
+    const decimal = number - base;
 
-  if (decimal === 0.25) return `${base}/${base + 0.5}`;
-  if (decimal === 0.75) return `${base + 0.5}/${base + 1}`;
-  
-  return `${number}`;
+    if (decimal === 0.25 || decimal === -0.25) {
+        return `${base}/3.5`;
+    }
+
+    if (decimal === 0.75 || decimal === -0.75) {
+        return `${base + 0.5}/${base + 1}`;
+    }
+
+    return `${number}`;
 }
+  
+  
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
